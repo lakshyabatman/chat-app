148bfc636df9c6645792ecf70fd6b84c2aadb048
lakshyabatman <lakshya.khera@gmail.com>
2018-11-02 00:29:40 +0530

    Changed port in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@
 const port=process.env.PORT || 3000;
  var app=express();
  app.use(express.static(publicPath));
- app.listen(3000,()=>{
+ app.listen(port,()=>{
      console.log(`Server is up! on ${port}`);
  })
  
